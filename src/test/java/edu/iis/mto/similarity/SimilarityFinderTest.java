c4f6108ea550742605b0e277c5cb71fc587ea9fa
H22A5 <bartlomiej.pajak20@gmail.com>
2021-03-22 18:27:08 +0100

    Added test for checking if finder returns zero when first sequence is not empty and second sequence is empty.

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -56,4 +56,15 @@ class SimilarityFinderTest {
 
         assertEquals(0, result);
     }
+
+    @Test
+    public void shouldReturnZeroWhenFirstSequenceLengthIsGreaterThanZeroAndSecondSequenceLengthIsZero() {
+        SimilarityFinder finder = new SimilarityFinder((elem, sequence) -> SearchResult.builder().withFound(false).build());
+        int[] seq1 = {1, 2};
+        int[] seq2 = {};
+
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(0, result);
+    }
 }
